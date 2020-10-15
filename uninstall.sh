7730e3ace2609325b10d5de5bc777cf4cd6f8f55
Greg Saltis <gregorysaltis@hotmail.com>
2020-10-15 13:35:55 -0400

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+rm -rf /etc/init.d/NCUSim.rc
+rm -rf /etc/rc1.d/S99NCUSim
+rm -rf /etc/rc2.d/S99NCUSim
+rm -rf /etc/rc3.d/S99NCUSim
+rm -rf /etc/rc4.d/S99NCUSim
+rm -rf /etc/rc5.d/K99NCUSim
+rm -rf /etc/rc6.d/K99NCUSim
